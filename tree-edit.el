1a035f14597bb7ab51bfe7a3dbf4e88bd5cd5f56
Ethan Leba <ethanleba5@gmail.com>
2023-11-23 14:45:38 -0500

    docs: interface is kinda stable?
    don't think i'll really be changing the base functions of tree-edit

--- tree-edit.el
+++ tree-edit.el
@@ -15,8 +15,7 @@
 ;; Provides a set of functions for structural editing or refactoring in any
 ;; language supported by tree-sitter.
 ;;
-;; The interface for this package is currently unstable, developing against it is
-;; unadvised!
+;; The interface for this package is moderately stable, but no promises ;)
 ;;
 ;; See `evil-tree-edit' if you're looking for a complete editing package.
 ;;
